fbc00be705351621a4c48e2c6e7f2f9ecb6fb4d5
Markus Sanders <markus.sanderst@gmail.com>
2019-05-01 00:39:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  output = ""
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return "#{array[0]} and #{array[1]}"
+  else 
+    i = 0;
+    while i < array.length - 1
+      output += array[i] + ", " 
+      i += 1
+    end
+    output += "and #{array.last}"
+    return output
+  end
 end
